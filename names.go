abc12f945e691d2247795ecadb9bca2e084225f5
Chris Laprun <metacosm@gmail.com>
2019-12-18 18:20:37 +0100

    feat: do not extend DependentResourceHelper anymore, use HalkyonResource
    Finally results in being able to create Postgres resources in Halkyon from
    the plugin!

--- names.go
+++ names.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"halkyon.io/api/v1beta1"
 	framework "halkyon.io/operator-framework"
 )
 
-func PostgresName(owner framework.Resource) string {
+func PostgresName(owner v1beta1.HalkyonResource) string {
 	return framework.DefaultDependentResourceNameFor(owner)
 }
 
-func ServiceAccountName(owner framework.Resource) string {
+func ServiceAccountName(owner v1beta1.HalkyonResource) string {
 	return PostgresName(owner) // todo: fix me
 }
