d173eb18e6416fbfe9e55ed20135c29193415ecd
Chris Laprun <metacosm@gmail.com>
2020-01-02 23:10:41 +0100

    feat: adapt to be framework and plugin architecture

--- rolebinding.go
+++ rolebinding.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"halkyon.io/api/v1beta1"
 	framework "halkyon.io/operator-framework"
 	authorizv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -10,33 +11,26 @@ type roleBinding struct {
 	framework.RoleBinding
 }
 
-func newRoleBinding(owner framework.Resource) roleBinding {
+func newRoleBinding(owner v1beta1.HalkyonResource) roleBinding {
 	generic := framework.NewOwnedRoleBinding(owner,
 		func() string { return "use-scc-privileged" },
-		func() string { return newRole(owner).Name() },
-		func() string {
-			p := newPostgres()
-			p.SetOwner(owner)
-			return p.Name()
-		})
+		func() string { return roleNamer() },
+		func() string { return newPostgres(owner).Name() })
 	rb := roleBinding{RoleBinding: generic}
-	generic.SetDelegate(rb)
 	return rb
 }
 
-func (res roleBinding) NewInstanceWith(owner framework.Resource) framework.DependentResource {
-	return newRoleBinding(owner)
-}
-
-func (res roleBinding) Build() (runtime.Object, error) {
-	build, err := res.RoleBinding.Build()
+func (res roleBinding) Build(empty bool) (runtime.Object, error) {
+	build, err := res.RoleBinding.Build(empty)
 	if err != nil {
 		return nil, err
 	}
 
 	ser := build.(*authorizv1.RoleBinding)
-	owner := res.Owner()
-	ser.Subjects = append(ser.Subjects, authorizv1.Subject{Kind: "ServiceAccount", Name: PostgresName(owner), Namespace: owner.GetNamespace()})
+	if !empty {
+		owner := res.Owner()
+		ser.Subjects = append(ser.Subjects, authorizv1.Subject{Kind: "ServiceAccount", Name: PostgresName(owner), Namespace: owner.GetNamespace()})
+	}
 
 	return ser, nil
 }
