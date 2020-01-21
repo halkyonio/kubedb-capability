e4ec99a6576dc10c7dd7b0552a1790e88c831c97
Chris Laprun <metacosm@gmail.com>
2020-01-21 21:55:07 +0100

    feat: use simplified server startup from latest plugins

--- cmd/kubedb-capability/plugin.go
+++ cmd/kubedb-capability/plugin.go
@@ -1,27 +1,12 @@
 package main
 
 import (
-	"encoding/gob"
-	"github.com/hashicorp/go-plugin"
 	"halkyon.io/kubedb-capability/pkg/plugin/mongodb"
 	"halkyon.io/kubedb-capability/pkg/plugin/mysql"
 	"halkyon.io/kubedb-capability/pkg/plugin/postgresql"
 	plugins "halkyon.io/plugins/capability"
-	kubedbv1 "kubedb.dev/apimachinery/apis/kubedb/v1alpha1"
-	"os"
-	"path/filepath"
 )
 
 func main() {
-	gob.Register(kubedbv1.Postgres{})
-	gob.Register(kubedbv1.PostgresList{})
-	pluginName := filepath.Base(os.Args[0])
-	p, err := plugins.NewAggregatePluginResource(postgresql.NewPluginResource(), mysql.NewPluginResource(), mongodb.NewPluginResource())
-	if err != nil {
-		panic(err)
-	}
-	plugin.Serve(&plugin.ServeConfig{
-		HandshakeConfig: plugins.Handshake,
-		Plugins:         map[string]plugin.Plugin{pluginName: &plugins.GoPluginPlugin{Delegate: p}},
-	})
+	plugins.StartPluginServerFor(postgresql.NewPluginResource(), mysql.NewPluginResource(), mongodb.NewPluginResource())
 }
